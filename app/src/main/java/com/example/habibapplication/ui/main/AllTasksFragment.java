e2ffa39475c1466f83189e4adb49e750a1bc1359
Owner <byanmiary1@gmail.com>
2019-10-27 12:19:53 +0200

    fixed all getter and setters add to string

--- app/src/main/java/com/example/habibapplication/ui/main/AllTasksFragment.java
+++ app/src/main/java/com/example/habibapplication/ui/main/AllTasksFragment.java
@@ -3,16 +3,22 @@ package com.example.habibapplication.ui.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.habibapplication.R;
+import com.example.habibapplication.data.MyTask;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -40,6 +46,23 @@ public class AllTasksFragment extends Fragment {
         FirebaseAuth auth=FirebaseAuth.getInstance();
         String uid = auth.getUid();
         DatabaseReference reference=database.getReference();
+
+        reference.child("tasks").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                for (DataSnapshot d:dataSnapshot.getChildren())
+                {
+                    MyTask t=d.getValue(MyTask.class);
+                    Log.d("MYTASK",t.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
 
